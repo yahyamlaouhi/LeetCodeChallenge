6e204112de60b083f3ada3a97974eee6da981bd3
Yahya Mlaouhi <57452767+yahyamlaouhi@users.noreply.github.com>
2022-08-07 15:00:42 +0100

    Time: 2 ms (84.17%), Space: 40.9 MB (77.59%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int mySqrt(int x) {
+        double a=Math.pow(x,0.5);
+        int result;
+        result=(int)a;
+        
+        
+        return result ;
+        
+    }
+}
